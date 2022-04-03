27645ea4006a7c93f97363b80def371b3318dde6
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2022-04-03 15:57:30 +0600

    end project

--- src/Compomants/About/About.js
+++ src/Compomants/About/About.js
@@ -1,11 +1,79 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
+  return (
+    <div className="grid md:grid-cols-2 md:px-20 my-10 gap-10">
+      <div>
+        <h1 className="text-4xl mb-5 text-slate-400">Basic Questions</h1>
         <div>
-            <h1>from about</h1>
+          <h3 className="text-xl my-3">What is accessibility?</h3>
+          <p className="text-sm">
+            This article starts off the module with a good look at what
+            accessibility is — this includes what groups of people we need to
+            consider and why, what tools different people use to interact with
+            the web, and how we can make accessibility part of our web
+            development workflow.
+          </p>
         </div>
-    );
+        <div>
+          <h3 className="text-xl my-3">
+            HTML: A good basis for accessibility?
+          </h3>
+          <p className="text-sm">
+            A great deal of web content can be made accessible just by making
+            sure the correct HTML elements are always used for the correct
+            purpose.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-xl my-3">
+            CSS and JavaScript accessibility best practices?
+          </h3>
+          <p className="text-sm">
+            CSS and JavaScript, when used properly, also have the potential to
+            allow for accessible web experiences, but if misused they can
+            significantly harm accessibility. This article outlines some CSS and
+            JavaScript best practices that should be considered to ensure that
+            even complex content is as accessible as possible.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-4xl mb-5 text-slate-400">Advanced Questions</h1>
+
+        <div>
+          <h3 className="text-xl my-3">
+            WAI-ARIA basics, WAI-ARIA basics, WAI-ARIA basics, WAI-ARIA basics?
+          </h3>
+          <p className="text-sm">
+            Following on from the previous article, sometimes making complex UI
+            controls that involve unsemantic HTML and dynamic JavaScript-updated
+            content can be difficult. WAI-ARIA is a technology that can help
+            with such problems by adding in further semantics that browsers and
+            assistive technologies can recognize and use to let users know what
+            is going on. Here we'll show how to use it at a basic level to
+            improve accessibility.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-xl my-3">Accessible multimedia?</h3>
+          <p className="text-sm">
+            Another category of content that can create accessibility problems
+            is multimedia — video, audio, and image content need to be given
+            proper textual alternatives, so they can be understood by assistive
+            technologies and their users. This article shows how.
+          </p>
+        </div>
+        <div>
+          <h3 className="text-xl my-3">Mobile accessibility?</h3>
+          <p className="text-sm">
+            With web access on mobile devices being so popular, and popular
+            platforms such as iOS and Android.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/hooks/getAllCoin.js
+++ src/hooks/getAllCoin.js
@@ -4,7 +4,7 @@ const useProducts = () => {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false"
     )
       .then((res) => res.json())
       .then((data) => setCoins(data));
