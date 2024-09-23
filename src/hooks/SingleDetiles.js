ac16a9bc0d331803166ffac2ca67c58f4f97a02a
Md Shihab Uddin <uddinmdshihab452@gmail.com>
2024-09-23 20:06:02 +0600

    add url on env file

--- src/hooks/SingleDetiles.js
+++ src/hooks/SingleDetiles.js
@@ -1,23 +1,18 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-const useDetails =()=>{
-    const {id}=useParams()
-    const [coin,setCoin]=useState([])
-useEffect(()=>{
+const useDetails = () => {
+  const { id } = useParams();
+  const [coin, setCoin] = useState([]);
+  useEffect(() => {
+    const url = `${process.env.REACT_APP_BASE_URL}/${id}`;
 
-const url=`https://api.coingecko.com/api/v3/coins/${id}`
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setCoin(data));
+  }, [id]);
 
-fetch(url)
-.then(res=>res.json())
-.then(data =>setCoin(data))
+  return [coin, setCoin];
+};
 
-
-
-},[id])
-
-return [coin,setCoin]
-    
-}
-
-export default useDetails
+export default useDetails;
--- src/hooks/getAllCoin.js
+++ src/hooks/getAllCoin.js
@@ -4,13 +4,13 @@ const useProducts = () => {
   const [coins, setCoins] = useState([]);
   useEffect(() => {
     fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false"
+      `${process.env.REACT_APP_BASE_URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false`
     )
       .then((res) => res.json())
       .then((data) => setCoins(data));
   }, []);
 
-  return [coins, setCoins]
+  return [coins, setCoins];
 };
 
 export default useProducts;
